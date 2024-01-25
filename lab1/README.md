fca36f34d9ba03157a2961eea3841167f7d30469
kjjohnsen <kjjohnsen@users.noreply.github.com>
2024-01-25 08:22:32 -0500

    complete lab 1 up until reflection

---
+++ lab1/README.md
@@ -0,0 +1,4 @@
+# Kyle Johnsen
+# Lab 1
+## Completion
+I completed all parts of this lab.
---
+++ lab1/lab1.py
@@ -0,0 +1,11 @@
+# Kyle Johnsen
+# Lab 1
+f = open('data.txt').read()
+content = f.read()
+num_lines = len(content.split('\n'))
+num_words = len(content.split()) # splits on any whitespace
+num_characters = len(content)
+print(num_lines, "lines,", num_words, "words, and", num_characters, "characters")
+f.close()
+
+
